3bd55ef4f577717d736bbb56b55782c5e27a34c8
aphaea <afaias.revival@gmail.com>
2016-12-20 19:54:49 +0100

    Add files via upload

---
+++ random_fun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+# Reservoir sampling of  multiple lines uniformly without replacement
+# running on a single machine
+import sys
+import random
+
+random.seed(10)
+reservoir_l = []
+line_num = 0
+
+for line in sys.stdin:
+        if line_num < 100:
+                reservoir_l.append(line.strip())
+        else:
+                prob = random.randint(0,line_num)
+                if prob < 100:
+                        reservoir_l[prob] = line.strip()
+        line_num += 1
+
+for l in reservoir_l:
+        print(l)
